073f29a37a93dcbc414bc51f8b30a18ae7d6aee8
ryukenlovestocode <shubhamsinha5632@gmail.com>
2026-01-28 16:21:11 +0530

    fixed music

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.reactions = True
 intents.members = True
+intents.voice_states = True
 
 # ---------- BOT ----------
 bot = commands.Bot(
